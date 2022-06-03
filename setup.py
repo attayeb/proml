6c27139e0106ea56c63fd7be17b7877a44c83933
attayeb <attayebmohsen@gmail.com>
2022-06-03 11:36:34 +0900

    h

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Proml',
+      version='0.1',
+      description='ProML',
+      author='Attayeb Mohsen',
+      author_email='',
+      url='',
+      packages=['proml', 'proml.functions'],
+     )
