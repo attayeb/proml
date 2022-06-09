05f073b6b5e4b3aa0b2ca2ae29772246d7b78c56
attayeb <attayebmohsen@gmail.com>
2022-06-09 17:11:50 +0900

    more colors, more metrics

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import setuptools
 
 setup(name='Proml',
-      version='0.0.1',
+      version='0.0.2',
       description='ProML',
       author='Attayeb Mohsen',
       author_email='',
